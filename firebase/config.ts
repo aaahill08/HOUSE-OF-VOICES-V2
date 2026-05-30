cfce51e411f2d55e5441eac94cdf5cd37851148f
Aahil <aahil@Aahils-MacBook-Air.local>
2026-05-30 18:37:26 +0530

    Fix Firebase auth persistence

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,15 +1,37 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
+
+import {
+  getAuth,
+  browserLocalPersistence,
+  setPersistence,
+} from "firebase/auth";
+
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_API_KEY,
+
+  authDomain:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+
+  projectId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+
+  storageBucket:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+
   messagingSenderId:
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    process.env
+      .NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+
+  appId:
+    process.env
+      .NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app =
@@ -17,5 +39,13 @@ const app =
     ? initializeApp(firebaseConfig)
     : getApps()[0];
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const auth =
+  getAuth(app);
+
+setPersistence(
+  auth,
+  browserLocalPersistence
+).catch(console.error);
+
+export const db =
+  getFirestore(app);
